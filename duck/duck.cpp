d609ad32715f1b44a0b633e9fbcf3761a94f0ad0
Ravi Kumar Sharma <rasharma@micron.com>
2022-05-27 08:34:11 +0530

    design: code optimization for runtime polymorphism
    No need to duplciate code in derived class(es). Abstract class
    can set the pointer to fly and quack behaviour.
    Now, mallard and rubber ducks can perform run time polymorphism.

--- duck/duck.cpp
+++ duck/duck.cpp
@@ -9,11 +9,17 @@ class Fly_behaviour {
 
 class Fly_withwings: public Fly_behaviour {
 	public:
-		void fly();
+		void fly()
+		{
+			cout << "I am flying" <<"\n";
+		}
 };
 class Fly_withoutwings: public Fly_behaviour {
 	public:
-		void fly();
+		void fly()
+		{
+			cout << "I am not flying!burrh" << "\n";
+		}
 };
 
 class Quack_behaviour {
@@ -45,30 +51,25 @@ class Mutequack:public Quack_behaviour {
 class Duck {
 	public:
 		Quack_behaviour *qb;
+		Fly_behaviour *fb;
 
 		Duck() {}
 		~Duck() {}
-		virtual void set_quack_behaviour(Quack_behaviour *_qbb) = 0;
-		virtual void perform_quack()
+		void set_quack_b(Quack_behaviour *_qb) 
 		{
-			std::cout << "abstract! can't quak quack" << "\n";
+			qb = _qb;
 		}
-		virtual void perform_fly()
+		void set_fly_b(Fly_behaviour *_fb) 
 		{
+			fb = _fb;
+		}
 
-			std::cout<<"abstract! can't fly" << "\n";
-		}
-		virtual void display()
-		{
-			std::cout<<"abstract! I am abstract" << "\n";
-		}
+		virtual void perform_quack() = 0;
+		virtual void perform_fly() = 0;
+		virtual void display() = 0;
 };
 class Mallad_duck : public  Duck {
 	public:
-		void set_quack_behaviour(Quack_behaviour *_qbb)
-		{
-			qb = _qbb;
-		}
 
 		void display();
 		void perform_quack();
@@ -86,17 +87,14 @@ void Mallad_duck::perform_quack()
 }
 void Mallad_duck::perform_fly()
 {
-	std::cout<<"flying!" << "\n";
+	//std::cout<<"flying!" << "\n";
+	fb->fly();
 }
 class Rubber_duck : public  Duck {
 	public:
 		void display();
 		void perform_quack();
 		void perform_fly();
-		void set_quack_behaviour(Quack_behaviour *_qbb)
-		{
-			qb = _qbb;
-		}
 
 };
 
@@ -106,11 +104,14 @@ void Rubber_duck::display()
 }
 void Rubber_duck::perform_quack()
 {
-	std::cout<<"squeak squeak!" << "\n";
+	//std::cout<<"squeak squeak!" << "\n";
+	qb->quack();
 }
 void Rubber_duck::perform_fly()
 {
-	std::cout<<"no flying!" << "\n";
+	//std::cout<<"no flying!" << "\n";
+	fb->fly();
+
 }
 
 int main()
@@ -118,18 +119,28 @@ int main()
 	Duck* mallad = new Mallad_duck();
 	Duck* rubber = new Rubber_duck();
 	Quack_behaviour *qb = new Quack();
+	Quack_behaviour *qb_1 = new Squeak();
+	Fly_behaviour *fb = new Fly_withwings();
+	Fly_behaviour *fb_1 = new Fly_withoutwings();
 
 	mallad->display();
-	mallad->set_quack_behaviour(qb);
+	mallad->set_quack_b(qb);
+	mallad->set_fly_b(fb);
 	mallad->perform_fly();
 	mallad->perform_quack();
 
-	//rubber->display();
-	//rubber->perform_fly();
-	//rubber->perform_quack();
+	rubber->display();
+	rubber->set_quack_b(qb_1);
+	rubber->set_fly_b(fb_1);
+	rubber->perform_fly();
+	rubber->perform_quack();
 
 	free(mallad);
 	free(rubber);
+	free(qb);
+	free(qb_1);
+	free(fb);
+	free(fb_1);
 
 	return 0;
 
